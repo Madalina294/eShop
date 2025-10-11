4d7e8f739cbb784a36b218d98e2eaaa4b3233af9
Madalina294 <puicanmadalinamaria@gmail.com>
2025-10-11 20:04:50 +0300

    Linked to a new database

--- Backend/src/main/java/com/app_template/App_Template/EShopApplication.java
+++ Backend/src/main/java/com/app_template/App_Template/EShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppTemplateApplication {
+public class EShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppTemplateApplication.class, args);
+		SpringApplication.run(EShopApplication.class, args);
 	}
 
 }
